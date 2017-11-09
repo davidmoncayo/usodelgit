701d0a2eeea3adc586e657258de8f659e6dd0fab
DavidEvil <nathaly9929@gmail.com>
2017-11-09 08:34:25 -0500

    primer repositorio

---
+++ git.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+		<html>
+		<head>
+			<link rel="stylesheet" type="text/css" href="s.css">
+			<title>hola</title>
+			<div class="david">
+		</head>
+		<body>
+			<h1>este es mi git</h1>
+			<h2>este es un cambio</h2>
+			<h3>el tercer cambio</h3>
+			<h4>el cuarto punto</h4>
+			<h5>quinto</h5>
+			<h6>se agrego el 6 h6</h6>
+			</div>
+		</body>
+		</html>		
